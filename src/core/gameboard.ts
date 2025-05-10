08e09e318530daee4d6b08ffe68cd2e3c1f369d2
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-10 15:26:03 -0300

    feat: implement Gameboard factory function with its tests

---
+++ src/core/gameboard.ts
@@ -0,0 +1,103 @@
+import {
+  CellState,
+  newCell,
+  type Cell,
+  type CellStateType,
+  type Ship,
+} from "@/core";
+
+export interface Coordinates {
+  x: number;
+  y: number;
+}
+
+interface Gameboard {
+  placeShip: (ship: Ship, coords: Coordinates, isVertical: boolean) => void;
+  getCellState: (coords: Coordinates) => CellStateType;
+  receiveAttack: (coords: Coordinates) => CellStateType;
+  getShipByCoords: (coords: Coordinates) => PlacedShip | null;
+  areAllShipsSunk: () => boolean;
+  getSize: () => number;
+}
+
+export interface PlacedShip {
+  ship: Ship;
+  coords: Array<Coordinates>;
+  isVertical: boolean;
+}
+
+const areCoordsEqual = (coords1: Coordinates, coords2: Coordinates) => {
+  return coords1.x === coords2.x && coords1.y === coords2.y;
+};
+
+export const newGameboard = (size: number = 10): Gameboard => {
+  const _size = size;
+  const _board: Array<Array<Cell>> = Array.from({ length: _size }, () =>
+    Array.from({ length: _size }, () => newCell()),
+  );
+  const _placedShips: Array<PlacedShip> = [];
+
+  const getShipByCoords = (coords: Coordinates): PlacedShip | null => {
+    const placedShip = _placedShips.find((placeShip) =>
+      placeShip.coords.some((coord) => areCoordsEqual(coord, coords)),
+    );
+    if (!placedShip) return null;
+
+    return placedShip;
+  };
+
+  const getSize = () => _size;
+
+  const placeShip = (ship: Ship, coords: Coordinates, isVertical: boolean) => {
+    const { x, y } = coords;
+    const shipCellsCords: Array<Coordinates> = [];
+    for (let i = 0; i < ship.getLength(); i++) {
+      const newX = isVertical ? x : x + i;
+      const newY = isVertical ? y + i : y;
+      const followedCell = _board[newX][newY];
+      followedCell.setState(CellState.ship as CellStateType);
+      shipCellsCords.push({ x: newX, y: newY });
+    }
+    _placedShips.push({ ship, coords: shipCellsCords, isVertical });
+  };
+
+  const receiveAttack = ({ x, y }: Coordinates) => {
+    const cell = _board[x][y];
+    const placedShip = getShipByCoords({ x, y });
+    if (placedShip) {
+      placedShip.ship.hit();
+
+      if (placedShip.ship.isSunk()) {
+        placedShip.coords.forEach((coord) => {
+          _board[coord.x][coord.y].setState(CellState.sunk as CellStateType);
+        });
+      } else {
+        cell.setState(CellState.hit as CellStateType);
+      }
+
+      return cell.getState();
+    }
+
+    cell.setState(CellState.miss as CellStateType);
+    return cell.getState();
+  };
+
+  const getCellState = (coords: Coordinates): CellStateType => {
+    const { x, y } = coords;
+    const cell = _board[x][y];
+    return cell.getState();
+  };
+
+  const areAllShipsSunk = () => {
+    return _placedShips.every((ship) => ship.ship.isSunk());
+  };
+
+  return {
+    getSize,
+    placeShip,
+    getCellState,
+    receiveAttack,
+    getShipByCoords,
+    areAllShipsSunk,
+  };
+};
---
+++ src/test/gameboard.test.ts
@@ -0,0 +1,57 @@
+import { CellState, newShip } from "@/core";
+import { newGameboard } from "@/core";
+import { expect, test } from "vitest";
+
+test("get cell state passing coordinates", () => {
+  const gameboard = newGameboard();
+  const cellState = gameboard.getCellState({ x: 0, y: 0 });
+  expect(cellState).toBe(CellState.empty);
+});
+
+test("get state of a cells that are part of a ship", () => {
+  const gameboard = newGameboard();
+  const ship = newShip(2);
+  gameboard.placeShip(ship, { x: 0, y: 0 }, false);
+  const cell1State = gameboard.getCellState({ x: 0, y: 0 });
+  const cell2State = gameboard.getCellState({ x: 1, y: 0 });
+  expect(cell1State).toBe(CellState.ship);
+  expect(cell2State).toBe(CellState.ship);
+});
+
+test("make an attack to an empty cell of the gameboard", () => {
+  const gameboard = newGameboard();
+  gameboard.receiveAttack({ x: 0, y: 0 });
+  const cellState = gameboard.getCellState({ x: 0, y: 0 });
+  expect(cellState).toBe(CellState.miss);
+});
+
+test("make an attack to a ship cell of the gameboard", () => {
+  const gameboard = newGameboard();
+  const ship = newShip(2);
+  gameboard.placeShip(ship, { x: 0, y: 0 }, false);
+  gameboard.receiveAttack({ x: 0, y: 0 });
+  const cellState = gameboard.getCellState({ x: 0, y: 0 });
+  expect(cellState).toBe(CellState.hit);
+});
+
+test("make an attack to all cells of a ship to sink it", () => {
+  const gameboard = newGameboard();
+  const ship = newShip(2);
+  gameboard.placeShip(ship, { x: 0, y: 0 }, false);
+  gameboard.receiveAttack({ x: 0, y: 0 });
+  gameboard.receiveAttack({ x: 1, y: 0 });
+  const cell1State = gameboard.getCellState({ x: 0, y: 0 });
+  const cell2State = gameboard.getCellState({ x: 1, y: 0 });
+  expect(cell1State).toBe(CellState.sunk);
+  expect(cell2State).toBe(CellState.sunk);
+});
+
+test("check if all ships are sunk", () => {
+  const gameboard = newGameboard();
+  const ship = newShip(2);
+  gameboard.placeShip(ship, { x: 0, y: 0 }, false);
+  expect(gameboard.areAllShipsSunk()).toBe(false);
+  gameboard.receiveAttack({ x: 0, y: 0 });
+  gameboard.receiveAttack({ x: 1, y: 0 });
+  expect(gameboard.areAllShipsSunk()).toBe(true);
+});
