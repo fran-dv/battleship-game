5541471eec5291ec8e8c98378c18acae6ace4496
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-13 08:40:12 -0300

    feat: centralize html attrs in a file

---
+++ src/ui/html-attrs.ts
@@ -0,0 +1,11 @@
+export const IDS = {
+  Root: "root",
+} as const;
+
+export const DataClickAttrs = {
+  LaunchGame: "launch-game",
+  OnePlayer: "one-player",
+  TwoPlayers: "two-players",
+  ToggleSoundEffects: "toggle-fx",
+  ToggleBackgroundMusic: "toggle-background-music",
+} as const;
