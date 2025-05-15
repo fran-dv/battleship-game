dc88c7055aee4b8d6597dcb128df493ef9137d52
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-15 15:55:30 -0300

    feat: implement random coords generator function

--- src/core/coordinates.ts
+++ src/core/coordinates.ts
@@ -6,3 +6,16 @@ export interface Coordinates {
 export const areCoordsEqual = (coords1: Coordinates, coords2: Coordinates) => {
   return coords1.x === coords2.x && coords1.y === coords2.y;
 };
+
+export const getRandomCoords = (
+  amount: number,
+  gameboardSize: number,
+): Array<Coordinates> => {
+  const coords: Array<Coordinates> = [];
+  for (let i = 0; i < amount; i++) {
+    const x = Math.floor(Math.random() * gameboardSize);
+    const y = Math.floor(Math.random() * gameboardSize);
+    coords.push({ x, y });
+  }
+  return coords;
+};
