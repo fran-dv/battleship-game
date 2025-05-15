014392268543ed7e2c37864480b20c21d726847a
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-15 16:27:57 -0300

    feat/fix: add news views to the main click events handler

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import "./index.css";
 import { DataClickAttrs, IDS, initViews } from "@/ui";
-import { Header } from "@/ui";
+import { Footer } from "@/ui";
+import { initGameSession, newPlayer, type GameSession } from "@/core";
+import { generateDiv } from "@fran-dv/ui-components";
+import styles from "./main.module.css";
 
 const main = document.getElementById(IDS.Root);
+const parent = generateDiv({ classes: [styles.parent] });
 
 if (!main) {
   throw new Error("Main element not found");
 }
+main.appendChild(parent);
 
-const Views = initViews(main);
+const Views = initViews(parent);
+let CurrentGameSession: GameSession | null = null;
 
 const clickHandler = (e: MouseEvent) => {
   const elem: HTMLElement | null = (e.target as HTMLElement).closest(
@@ -21,17 +27,29 @@ const clickHandler = (e: MouseEvent) => {
       Views.loadHomeView();
       break;
     case DataClickAttrs.OnePlayer:
-      Views.clickOnOnePlayerButton();
+      CurrentGameSession = initGameSession(newPlayer("Player one"));
+      Views.clickOnOnePlayerButton(CurrentGameSession);
       break;
     case DataClickAttrs.TwoPlayers:
-      Views.clickOnTwoPlayersButton();
+      CurrentGameSession = initGameSession(
+        newPlayer("Player one"),
+        newPlayer("Player two"),
+      );
+      Views.clickOnTwoPlayersButton(CurrentGameSession);
       break;
     case DataClickAttrs.ToggleSoundEffects:
-      Header?.clickOnMuteFx();
+      Footer?.clickOnMuteFx();
       break;
     case DataClickAttrs.ToggleBackgroundMusic:
-      console.log("mute music");
-      Header?.clickOnMuteMusic();
+      Footer?.clickOnMuteMusic();
+      break;
+    case DataClickAttrs.StartGame:
+      if (!CurrentGameSession) {
+        console.error("Active game session not found");
+        Views.loadHomeView();
+        return;
+      }
+      Views.loadGameSessionView(parent, CurrentGameSession);
       break;
   }
 };
