bec7c3d5d57bb3c76a106e274df3c82d290be922
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-15 16:15:22 -0300

    fix/style: remove not concerning logic of home view and enhance styles and responsiveness

--- src/ui/home/home.ts
+++ src/ui/home/home.ts
@@ -1,5 +1,6 @@
 import { generateDiv } from "@fran-dv/ui-components";
 import styles from "./home.module.css";
+import animations from "@/ui/animations/animations.module.css";
 import logo from "@/assets/image/logo.png";
 import { DataClickAttrs, SoundManager } from "@/ui";
 
@@ -9,14 +10,21 @@ const buttonTexts = {
 };
 
 export interface HomeHandlers {
-  clickOnOnePlayerButton: () => void;
-  clickOnTwoPlayersButton: () => void;
+  clickOnButton: () => void;
   destroy: () => void;
 }
 
-export const renderHomeView = (parent: HTMLElement): HomeHandlers => {
+export const renderHomeView = (
+  parent: HTMLElement,
+  delay: number = 0,
+): HomeHandlers => {
+  const footer = document.querySelector("footer");
+  if (footer) {
+    footer.classList.add(styles.footer);
+  }
   document.body.classList.add(styles.mainBackground);
   const container = generateDiv({ classes: [styles.container] });
+  container.classList.add(animations.fadeIn);
   const homeLogoSection = generateDiv({
     classes: [styles.battleshipLogoSection],
   });
@@ -42,24 +50,25 @@ export const renderHomeView = (parent: HTMLElement): HomeHandlers => {
   container.appendChild(homeButtonsSection);
   parent.appendChild(container);
 
-  const clickOnOnePlayerButton = () => {
-    SoundManager.clickOnButton();
-    // renderOnePlayerView(parent);
-  };
+  parent.classList.add(styles.parent);
 
-  const clickOnTwoPlayersButton = () => {
+  const clickOnButton = () => {
     SoundManager.clickOnButton();
-    // renderTwoPlayersView(parent);
   };
 
   const destroy = () => {
-    parent.removeChild(container);
+    container.classList.remove(animations.fadeIn);
+    container.classList.add(animations.fadeOut);
+    setTimeout(() => {
+      parent.classList.remove(styles.parent);
+      parent.removeChild(container);
+      footer?.classList.remove(styles.footer);
+    }, delay);
   };
 
   SoundManager.playBackgroundMusic();
   return {
-    clickOnOnePlayerButton,
-    clickOnTwoPlayersButton,
+    clickOnButton,
     destroy,
   };
 };
