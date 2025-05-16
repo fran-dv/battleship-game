99395460b25703640af8dca5005f9a336c01160c
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-16 06:29:43 -0300

    add ships images

--- src/core/game-rules.ts
+++ src/core/game-rules.ts
@@ -1,3 +1,9 @@
+import battleshipImage from "@/assets/image/ships/battleship-ship-horizontal.png";
+import carrierImage from "@/assets/image/ships/carrier-ship-horizontal.png";
+import cruiserImage from "@/assets/image/ships/cruiser-ship-horizontal.png";
+import destroyerImage from "@/assets/image/ships/destroyer-ship-horizontal.png";
+import patrolBoatImage from "@/assets/image/ships/patrol-boat-ship-horizontal.png";
+
 export const GameRules = {
   boardSize: 10,
   numberOfPlayers: 2,
@@ -10,31 +16,31 @@ export const GameRules = {
     {
       size: 5,
       name: "Carrier",
-      imageUrl: "",
+      imageUrl: carrierImage,
       className: "carrier",
     },
     {
       size: 4,
       name: "Battleship",
-      imageUrl: "",
+      imageUrl: battleshipImage,
       className: "battleship",
     },
     {
       size: 3,
       name: "Cruiser",
-      imageUrl: "",
+      imageUrl: cruiserImage,
       className: "cruiser",
     },
     {
       size: 3,
-      name: "Submarine",
-      imageUrl: "",
-      className: "submarine",
+      name: "Destroyer",
+      imageUrl: destroyerImage,
+      className: "destroyer",
     },
     {
       size: 2,
       name: "Patrol Boat",
-      imageUrl: "",
+      imageUrl: patrolBoatImage,
       className: "patrol-boat",
     },
   ],
