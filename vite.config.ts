398ae2f58c568bda9bb108d919c11c51221a0b6d
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-17 13:50:42 -0300

    install gh-pages and configure to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/battleship-game/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
